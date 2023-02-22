40dddcae4213d78305b73b618c6dca71e1a5b783
Davinson <doglasprincedavinson@gmail.com>
2023-02-22 22:58:12 +0100

    server work miss deletepost

--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -4,6 +4,8 @@ const postController = require("../controllers/post.controller.js")
 router.get("/", postController.allPosts);
 router.post("/",postController.addPost);
 
-router.get("/:id")
+router.get("/:id", postController.onePost);
+router.get("/search/:query", postController.searchPost);
+
 
 module.exports = router;
