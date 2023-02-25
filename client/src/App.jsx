28438eb212f5d6b0247da4a1c026e52316715a2e
Davinson <doglasprincedavinson@gmail.com>
2023-02-25 22:09:28 +0100

    create jsx pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,21 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Add from "./pages/Add";
+import Home from "./pages/Home";
+import Post from "./pages/Post";
+import Search from "./pages/Search";
+
 function App() {
   return (
-    <div className="App">Hello</div>
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path ="/" element={<Home />} />
+          <Route path ="/:id" element={<Post/>} />
+          <Route path ="/search" element={<Search/>} />
+          <Route path ="/add" element={<Add/>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
